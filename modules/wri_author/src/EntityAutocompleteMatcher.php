a1edb59bddf4d39cf1a2e8546965b91687e3170f
Maria Fisher <maria.fisher@thinkshout.com>
2022-03-04 13:38:49 -0800

    Issue #338 updating autocomplete matcher code to not pull in all entities and more match parent.

--- modules/wri_author/src/EntityAutocompleteMatcher.php
+++ modules/wri_author/src/EntityAutocompleteMatcher.php
@@ -35,41 +35,37 @@ class EntityAutocompleteMatcher extends EntityAutocompleteMatcherOrig {
    * Gets matched labels based on a given search string.
    */
   public function getMatches($target_type, $selection_handler, $selection_settings, $string = '') {
-
     $matches = [];
 
-    $options = [
-      'target_type'      => $target_type,
-      'handler'          => $selection_handler,
-      'handler_settings' => $selection_settings,
-    ];
-
+    $options = $selection_settings + [
+        'target_type' => $target_type,
+        'handler' => $selection_handler,
+      ];
     $handler = $this->selectionManager->getInstance($options);
 
     if (isset($string)) {
       // Get an array of matching entities.
       $match_operator = !empty($selection_settings['match_operator']) ? $selection_settings['match_operator'] : 'CONTAINS';
-      $entity_labels = $handler->getReferenceableEntities($string, $match_operator, 10);
+      $match_limit = isset($selection_settings['match_limit']) ? (int) $selection_settings['match_limit'] : 10;
+      $entity_labels = $handler->getReferenceableEntities($string, $match_operator, $match_limit);
 
       // Loop through the entities and convert them into autocomplete output.
       foreach ($entity_labels as $values) {
         foreach ($values as $entity_id => $label) {
-
           $entity = $this->entityTypeManager->getStorage($target_type)->load($entity_id);
           $entity = $this->entityRepository->getTranslationFromContext($entity);
-
           $type = !empty($entity->type->entity) ? $entity->type->entity->label() : $entity->bundle();
           $status = '';
           if ($entity->getEntityType()->id() == 'node') {
             $status = ($entity->isPublished()) ? ", Published" : ", Unpublished";
           }
-
-          $key = $label . ' (' . $entity_id . ')';
-          // Strip things like white spaces, line breaks and tags.
+          $key = "$label ($entity_id)";
+          // Strip things like starting/trailing white spaces, line breaks and
+          // tags.
           $key = preg_replace('/\s\s+/', ' ', str_replace("\n", '', trim(Html::decodeEntities(strip_tags($key)))));
           // Names containing commas or quotes must be wrapped in quotes.
           $key = Tags::encode($key);
-          $label = $label . ' (' . $entity_id . ') [' . $type . $status . ']';
+          $label = "$label ($entity_id) [$type $status]";
           $matches[] = ['value' => $key, 'label' => $label];
         }
       }
