f0b2cda3a0aacf9342c0004adc22a70ae538d08b
Maria Fisher <maria.fisher@thinkshout.com>
2022-04-20 10:11:06 -0700

    Issue #362 updating how the file gets called.

--- modules/wri_services_reader/src/Plugin/rest/resource/WriReaderRetrieveNodeResource.php
+++ modules/wri_services_reader/src/Plugin/rest/resource/WriReaderRetrieveNodeResource.php
@@ -159,7 +159,7 @@ class WriReaderRetrieveNodeResource extends ResourceBase {
             $media = Media::load($mid['target_id']);
             $fid = $media->field_media_file->target_id;
             $file = File::load($fid);
-            $files[]['url'] = $file->toUrl();
+            $files[]['url'] = \Drupal::service('file_url_generator')->generateAbsoluteString($file->getFileUri());
           }
           $node_array[$field_name] = [
             $language => $files,
