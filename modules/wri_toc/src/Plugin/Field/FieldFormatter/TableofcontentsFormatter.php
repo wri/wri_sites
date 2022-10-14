e6dddab1b8ab701e1403a271444f309b2de63e9d
Maria Fisher <maria.fisher@thinkshout.com>
2022-10-13 17:21:00 -0700

    Issue #768 adding microsite module.

--- modules/wri_toc/src/Plugin/Field/FieldFormatter/TableofcontentsFormatter.php
+++ modules/wri_toc/src/Plugin/Field/FieldFormatter/TableofcontentsFormatter.php
@@ -36,7 +36,8 @@ class TableofcontentsFormatter extends FormatterBase {
     $elements['menu'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Menu name'),
-      '#default_value' => $this->getSetting('menu'),
+      '#description' => $this->t('The dash-case name of the menu: ie page-hierarchies'),
+      '#default_value' => $this->getSetting('menu') ?? 'page-hierarchies',
     ];
 
     return $elements;
@@ -46,7 +47,7 @@ class TableofcontentsFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public function settingsSummary() {
-    $summary[] = $this->t('Menu: @menu', ['@foo' => $this->getSetting('menu')]);
+    $summary[] = $this->t('Menu: @menu', ['@menu' => $this->getSetting('menu')]);
     return $summary;
   }
 
@@ -57,10 +58,26 @@ class TableofcontentsFormatter extends FormatterBase {
     $element = [];
 
     foreach ($items as $delta => $item) {
+      $variables['menu_link'] = '';
+      $variables['menu_title'] = $this->getSetting('menu') ?? 'page-hierarchies';
+      $node = \Drupal::routeMatch()->getParameter('node');
+      if ($node instanceof \Drupal\node\NodeInterface) {
+        $variables['node_title'] = $node->getTitle();
+      }
+      // Get the menu link of the current node.
+      if($item->value == 'menu' && $entity = $item->getEntity()) {
+        $node_id = $item->getEntity()->id();
+        if ($node_id) {
+          $menu_link_manager = \Drupal::service('plugin.manager.menu.link');
+          $variables['menu_link'] = array_key_first($menu_link_manager->loadLinksByRoute('entity.node.canonical', ['node' => $node_id], $this->getSetting('menu')));
+        }
+      }
       $element[$delta] = [
-        '#markup' => $item->value,
+        '#theme' => 'table_of_contents',
+        '#elements' => $variables
       ];
     }
+
 
     return $element;
   }
