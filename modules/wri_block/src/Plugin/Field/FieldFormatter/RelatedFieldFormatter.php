34b0fcaaae55c300801b730956daf21fdb35ecf5
Maria Fisher <maria.fisher@thinkshout.com>
2022-10-27 14:34:34 -0700

    Issue #932 deprecating some code.

--- modules/wri_block/src/Plugin/Field/FieldFormatter/RelatedFieldFormatter.php
+++ modules/wri_block/src/Plugin/Field/FieldFormatter/RelatedFieldFormatter.php
@@ -13,67 +13,10 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @FieldFormatter(
  *   id = "related_field_formatter",
- *   label = @Translation("Related field formatter"),
+ *   label = @Translation("Deprecated Related Field Formatter"),
  *   field_types = {
  *     "entity_reference"
  *   }
  * )
  */
-class RelatedFieldFormatter extends EntityReferenceEntityFormatter {
-
-  /**
-   * Request stack.
-   *
-   * @var \Symfony\Component\HttpFoundation\RequestStack
-   */
-  protected $requestStack;
-
-  /**
-   * {@inheritDoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
-    $instance->requestStack = $container->get('request_stack');
-    return $instance;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function defaultSettings() {
-    return [
-      // Implement default settings.
-    ] + parent::defaultSettings();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsForm(array $form, FormStateInterface $form_state) {
-    return [
-      // Implement settings form.
-    ] + parent::settingsForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function viewElements(FieldItemListInterface $items, $langcode) {
-    if (isset($items) && $items->count()) {
-      return parent::viewElements($items, $langcode);
-    }
-    else {
-      $node = $this->requestStack->getCurrentRequest()->attributes->get('node');
-      if ($node) {
-        $view = Views::getView('related_content');
-        $view->setDisplay('block_1');
-        $view->setArguments([$node->id()]);
-        $build = $view->render();
-        $build['#field_name'] = $items->getName();
-        return $build;
-      }
-    }
-    return [];
-  }
-
-}
+class RelatedFieldFormatter extends EntityReferenceEntityFormatter { }
