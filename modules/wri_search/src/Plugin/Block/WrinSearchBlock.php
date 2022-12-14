8bc32d51c6c5920957a9ce43b768a8e4ffd504f2
Maria Fisher <maria.fisher@thinkshout.com>
2022-12-14 15:06:52 -0800

    Issue #132 removing the sfc for the search menu.

---
+++ modules/wri_search/src/Plugin/Block/WrinSearchBlock.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Drupal\wri_search\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityFormInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a wrin search block.
+ *
+ * @Block(
+ *   id = "single_file_component_block:wri_search_menu",
+ *   admin_label = @Translation("WRIN Search"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class WrinSearchBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'search_nav_title' => '',
+      'search_nav_description' => '',
+      'search_nav_label' => '',
+      'search_nav_id' => '',
+      'search_nav_submit' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $default_values = $this->configuration;
+    $form['search_nav_title'] = [
+      '#type'   => 'textfield',
+      '#title'  => $this->t('Search box title'),
+      '#default_value' => isset($default_values['search_nav_title']) ? $default_values['search_nav_title']  : 'Search WRI.org',
+    ];
+    $form['search_nav_description'] = [
+      '#type'   => 'textfield',
+      '#title'  => $this->t('Search box description'),
+      '#default_value' => isset($default_values['search_nav_description']) ? $default_values['search_nav_description']  : 'Not sure where to find something? Search all of the site\'s content.'
+    ];
+    $form['search_nav_label'] = [
+      '#type'   => 'textfield',
+      '#title'  => $this->t('Input label'),
+      '#default_value' => isset($default_values['search_nav_label']) ? $default_values['search_nav_label']  : 'What can we help you find?',
+    ];
+
+    if ($form_state->getFormObject() instanceOf EntityFormInterface) {
+      $nid = $form_state->getformObject()->getEntity()->id();
+    }
+    $form['search_nav_id'] = [
+      '#type'   => 'hidden',
+      '#default_value' => $nid,
+    ];
+    $form['search_nav_submit'] = [
+      '#type'   => 'textfield',
+      '#title'  => $this->t('Submit button text'),
+      '#default_value' => isset($default_values['search_nav_submit']) ? $default_values['search_nav_submit']  : 'Search',
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    foreach ($this->defaultConfiguration() as $key => $default) {
+      $value = $form_state->getValue($key);
+      $this->configuration[$key] = $value;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build['content'] = [
+      '#theme' => 'wri_search_menu',
+      '#search_nav_title' => $this->configuration['search_nav_title'],
+      '#search_nav_description' => $this->configuration['search_nav_description'],
+      '#search_nav_label' => $this->configuration['search_nav_label'],
+      '#search_nav_id' => $this->configuration['search_nav_id'],
+      '#search_nav_submit' => $this->configuration['search_nav_submit'],
+    ];
+    return $build;
+  }
+
+}
