613d705c54acdcd4866faa7b2a402d3d522afad0
Maria Fisher <maria.fisher@thinkshout.com>
2023-01-20 13:54:00 -0800

    Issue #142 Replacing spaces with hash signs in term names. If we ever have a term name with a hash sign we'll have to figure out a different symbol.

--- modules/wri_common/src/Plugin/views/argument/StringWithSpaceArgument.php
+++ modules/wri_common/src/Plugin/views/argument/StringWithSpaceArgument.php
@@ -15,13 +15,21 @@ use Drupal\views\Plugin\views\argument\StringArgument;
  * @ViewsArgument("string_with_space")
  */
 class StringWithSpaceArgument extends StringArgument {
+
+  /**
+   * {@inheritDoc}
+   */
   public static function breakString($str, $force_int = FALSE) {
-    // Change my spaces into another character.
-    $str = str_replace(' ', '&nbsp;', $str);
+    // Change my spaces into another character and then change back.
+    // Some day there may be a string with a # in it, but for now, this works.
+    $splitter = '#';
+
+    $str = str_replace(' ', $splitter, $str);
     $result = parent::breakString($str, $force_int);
-    foreach ($result['value'] as $key => $value) {
-      $result['value'][$key] = str_replace('&nbsp;', ' ', $value);
+    foreach ($result->value as $key => $value) {
+      $result->value[$key] = str_replace($splitter, ' ', $value);
     }
     return $result;
   }
+
 }
