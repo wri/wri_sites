eda424c8eb34cb6bfca6c6b2528026274080bb20
Maria Fisher <maria.fisher@thinkshout.com>
2023-01-25 09:09:25 -0800

    Issue #379 maintaining key as an int not string.

--- modules/wri_person/wri_person.post_update.php
+++ modules/wri_person/wri_person.post_update.php
@@ -58,7 +58,7 @@ function wri_person_post_update_person_grouping(&$sandbox) {
     // Figure out what the status is and set the content moderation
     // appropriately.
     if ($node->status->value == 0) {
-      $node->moderation_state->set('0','archived');
+      $node->moderation_state->set(0,'archived');
     }
     $node->save();
     $sandbox['current']++;
