575352ce0c881f674a096641844b5d986a1b35c3
Maria Fisher <maria.fisher@thinkshout.com>
2023-04-26 12:25:28 -0700

    Issue #1030 post-update hook updates.

--- modules/wri_narrative/wri_narrative.post_update.php
+++ modules/wri_narrative/wri_narrative.post_update.php
@@ -12,11 +12,12 @@ use Drupal\node\Entity\Node;
  */
 function wri_narrative_post_update_rewrite_narrative_taxonomies(&$sandbox) {
   if (!isset($sandbox['total'])) {
-    $nids = Drupal::database()->select('node__field_narrative_taxonomy', 'u')
+    $sandbox['total'] = Drupal::database()->select('node__field_narrative_taxonomy', 'u')
       ->condition('u.field_narrative_taxonomy_value', '%<a href="/node/%', 'LIKE')
       ->fields('u')
-      ->execute();
-    $sandbox['total'] = count($nids);
+      ->countQuery()
+      ->execute()
+      ->fetchField();
     $sandbox['current'] = 0;
 
     if (empty($sandbox['total'])) {
@@ -40,10 +41,19 @@ function wri_narrative_post_update_rewrite_narrative_taxonomies(&$sandbox) {
   foreach ($nids as $result) {
     $node = Node::load($result->entity_id);
     // Replace link strings with new values.
+    $node->field_narrative_taxonomy->setValue(str_replace(
+      ['<a href="/node/[node:field_projects:target_id]">[node:field_projects:entity]</a>',
+        '<a href="/node/[node:field_primary_contacts:target_id]">[node:field_primary_contacts:entity]</a>'],
+      ['[node:field_projects:entity:link]',
+        '[node:field_primary_contacts:entity:link]'],
+      $node->field_narrative_taxonomy->getValue()
+    ));
 
     // Save the node.
-    $node->save();$sandbox['current']++;
+    $node->save();
+    $sandbox['current']++;
   }
+
 
   \Drupal::messenger()
     ->addMessage($sandbox['current'] . ' persons processed.');
