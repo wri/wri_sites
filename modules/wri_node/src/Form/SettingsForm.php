6e8384389290603b19a3e3c4754e95754600fc5b
Maria Fisher <maria.fisher@thinkshout.com>
2023-05-08 15:54:18 -0700

    Issue #1030 adding the ability to configure the person link.

--- modules/wri_node/src/Form/SettingsForm.php
+++ modules/wri_node/src/Form/SettingsForm.php
@@ -6,6 +6,7 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Extension\ModuleExtensionList;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RequestContext;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -23,9 +24,10 @@ class SettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function __construct(ConfigFactoryInterface $config_factory, ModuleExtensionList $module_extension_list) {
+  public function __construct(ConfigFactoryInterface $config_factory, ModuleExtensionList $module_extension_list, RequestContext $request_context) {
     parent::__construct($config_factory);
     $this->moduleExtensionList = $module_extension_list;
+    $this->requestContext = $request_context;
   }
 
   /**
@@ -34,7 +36,8 @@ class SettingsForm extends ConfigFormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
-      $container->get('extension.list.module')
+      $container->get('extension.list.module'),
+      $container->get('router.request_context')
     );
   }
 
@@ -63,6 +66,22 @@ class SettingsForm extends ConfigFormBase {
       '#description' => $this->t('If checked, a Card for a Node with no existing Main Image will display a default image (based on the title of the Primary topic). If one is not found the default image at <a href="@href" target="_blank">default.jpg</a> will be used.', ['@href' => $url]),
       '#default_value' => $this->config('wri_node.settings')->get('use_fallback_image'),
     ];
+
+    $form['unpublished_person_phrase'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Unpublished Person phrase'),
+      '#default_value' => $this->config('wri_node.settings')->get('unpublished_person_phrase'),
+      '#required' => TRUE,
+    ];
+
+    $form['person_listing_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Person listing url'),
+      '#default_value' => $this->config('wri_node.settings')->get('person_listing_url'),
+      '#size' => 40,
+      '#description' => $this->t('If a narrative taxonomy contains a person link that has been unpublished, link to this url instead.'),
+      '#field_prefix' => $this->requestContext->getCompleteBaseUrl(),
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -72,6 +91,8 @@ class SettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('wri_node.settings')
       ->set('use_fallback_image', $form_state->getValue('use_fallback_image'))
+      ->set('unpublished_person_phrase', $form_state->getValue('unpublished_person_phrase'))
+      ->set('person_listing_url', $form_state->getValue('person_listing_url'))
       ->save();
     parent::submitForm($form, $form_state);
   }
