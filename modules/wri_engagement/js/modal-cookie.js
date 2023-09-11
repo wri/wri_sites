b2fc3a32eb5218eea87131c4e9776592cf145e92
Maria Fisher <maria.fisher@thinkshout.com>
2023-09-11 07:18:44 -0700

    Issue #301 pulling over smallest parts of https://github.com/wri/wri_sites/pull/10/files changes.

--- modules/wri_engagement/js/modal-cookie.js
+++ modules/wri_engagement/js/modal-cookie.js
@@ -14,51 +14,53 @@
   if (hasSignedUp || webformModalMissing) {
     $('.files-overlay-trigger-webform').addClass('files-overlay-trigger').removeClass('files-overlay-trigger-webform');
   }
-  // If you ask to show the files overlay from within an overlay, do it, and close the current modal.
+  // Reset when opening the popup.
   $('.files-overlay-trigger').click(function () {
-    // Close the modal.
-    $(this).parents('.spb-popup-main-wrapper').find('.spb_close').click();
+    // Show the files list and hide all other forms.
+    $('.files-modal-unregistered').hide();
+    $('.files-modal-confirmation').hide();
+    $('.files-modal-registered').show();
   });
 
   // Show or skip the registration form.
-  // var skipRegistration = Cookies.get('skip_registration');
-  // if (skipRegistration) {
-  //   // Hide the registration form, display the files.
-  //   $('.files-modal-unregistered').hide();
-  // }
-  // else {
-  //   // Hide the files list.
-  //   $('.files-modal-registered').hide();
-  //   // Email validation.
-  //   function validateEmail(email) {
-  //     let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  //     return re.test(email);
-  //   }
-  //   // Registration submit handler.
-  //   $('form#pardot-subscription').on('submit', function (e) {
-  //     e.preventDefault();
-  //     if (validateEmail(document.getElementById('email').value)) {
-  //       $.post('https://connect.wri.org/l/120942/2021-04-06/528nh6\n', $(this).serialize(), function (data) {
-  //         $('.files-modal-unregistered').hide();
-  //         $('.files-modal-registered').show();
-  //         Cookies.set('skip_registration', true);
-  //       }).fail(function() {
-  //         // This is executed when the call to mail.php failed.
-  //         console.log('registration failed');
-  //         $('.files-modal-unregistered').hide();
-  //         $('.files-modal-registered').show();
-  //         Cookies.set('skip_registration', true);
-  //       });
-  //     }
-  //     else {
-  //       $('#email').css({"border":"2px solid red"});
-  //     }
-  //   });
-  //   // Skip registration.
-  //   $('#skip-registration').on('click', function() {
-  //     $('.files-modal-unregistered').hide();
-  //     $('.files-modal-registered').show();
-  //     Cookies.set('skip_registration', true);
-  //   })
-  // }
+  var skipRegistration = Cookies.get('skip_registration');
+  if (skipRegistration) {
+    // Hide the registration form, display the files.
+    $('.files-modal-unregistered').hide();
+  }
+  else {
+    // Hide the files list.
+    $('.files-modal-registered').hide();
+    // Email validation.
+    function validateEmail(email) {
+      let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return re.test(email);
+    }
+    // Registration submit handler.
+    $('form#pardot-subscription').on('submit', function (e) {
+      e.preventDefault();
+      if (validateEmail(document.getElementById('email').value)) {
+        $.post('https://connect.wri.org/l/120942/2021-04-06/528nh6\n', $(this).serialize(), function (data) {
+          $('.files-modal-unregistered').hide();
+          $('.files-modal-registered').show();
+          Cookies.set('skip_registration', true);
+        }).fail(function() {
+          // This is executed when the call to mail.php failed.
+          console.log('registration failed');
+          $('.files-modal-unregistered').hide();
+          $('.files-modal-registered').show();
+          Cookies.set('skip_registration', true);
+        });
+      }
+      else {
+        $('#email').css({"border":"2px solid red"});
+      }
+    });
+    // Skip registration.
+    $('#skip-registration').on('click', function() {
+      $('.files-modal-unregistered').hide();
+      $('.files-modal-registered').show();
+      Cookies.set('skip_registration', true);
+    })
+  }
 })(jQuery, Drupal);
