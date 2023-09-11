510182cb46f93bcf486771f3859d15257584943d
Maria Fisher <maria.fisher@thinkshout.com>
2023-09-11 09:52:11 -0700

    Issue #301 going forward with the built-in dialog.

--- modules/wri_engagement/js/modal-cookie.js
+++ modules/wri_engagement/js/modal-cookie.js
@@ -5,62 +5,18 @@
   var existingModalSettings = drupalSettings.simple_popup_blocks.settings;
   // Loop through the existing modal settings and find one where the trigger is .files-overlay-trigger-webform
   for (var i in existingModalSettings) {
-    if (existingModalSettings[i].trigger_selector == '.files-overlay-trigger-webform') {
+    if (existingModalSettings[i].trigger_selector == '.webform-dialog') {
       webformModalMissing = false;
       break;
     }
   }
   // If the person has already signed up for email, go straight to the files overlay.
   if (hasSignedUp || webformModalMissing) {
-    $('.files-overlay-trigger-webform').addClass('files-overlay-trigger').removeClass('files-overlay-trigger-webform');
+    $('.files-overlay-trigger-webform').addClass('files-overlay-trigger').removeClass('webform-dialog');
   }
-  // Reset when opening the popup.
-  $('.files-overlay-trigger').click(function () {
-    // Show the files list and hide all other forms.
-    $('.files-modal-unregistered').hide();
-    $('.files-modal-confirmation').hide();
-    $('.files-modal-registered').show();
-  });
 
-  // Show or skip the registration form.
-  var skipRegistration = Cookies.get('skip_registration');
-  if (skipRegistration) {
-    // Hide the registration form, display the files.
-    $('.files-modal-unregistered').hide();
-  }
-  else {
-    // Hide the files list.
-    $('.files-modal-registered').hide();
-    // Email validation.
-    function validateEmail(email) {
-      let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return re.test(email);
-    }
-    // Registration submit handler.
-    $('form#pardot-subscription').on('submit', function (e) {
-      e.preventDefault();
-      if (validateEmail(document.getElementById('email').value)) {
-        $.post('https://connect.wri.org/l/120942/2021-04-06/528nh6\n', $(this).serialize(), function (data) {
-          $('.files-modal-unregistered').hide();
-          $('.files-modal-registered').show();
-          Cookies.set('skip_registration', true);
-        }).fail(function() {
-          // This is executed when the call to mail.php failed.
-          console.log('registration failed');
-          $('.files-modal-unregistered').hide();
-          $('.files-modal-registered').show();
-          Cookies.set('skip_registration', true);
-        });
-      }
-      else {
-        $('#email').css({"border":"2px solid red"});
-      }
-    });
-    // Skip registration.
-    $('#skip-registration').on('click', function() {
-      $('.files-modal-unregistered').hide();
-      $('.files-modal-registered').show();
-      Cookies.set('skip_registration', true);
-    })
-  }
+  $('#skip-registration').on('click', function() {
+    $('.files-modal-registered').show();
+    Cookies.set('has_signed_up', true);
+  });
 })(jQuery, Drupal);
