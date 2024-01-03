0d46218bfd6bf2a9bdfcb35b2b0246c050d4182a
Maria Fisher <maria.fisher@thinkshout.com>
2024-01-03 08:38:09 -0800

    Issue #228 more work to make the ckeditor5 template work.

--- themes/custom/ts_wrin/webpack.config.js
+++ themes/custom/ts_wrin/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env, argv) => {
     devtool: isDevMode ? "source-map" : false,
     entry: {
       main: ["./js/main.js", "./sass/style.scss"],
-      ckeditor: ["./sass/ckeditor.scss"]
+      ckeditor5: ["./sass/ckeditor.scss"]
     },
     module: {
       rules: [
