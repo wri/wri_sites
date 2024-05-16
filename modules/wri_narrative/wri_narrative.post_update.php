536f0aab39752c8e9e11f199e4777d2a61e67f7a
Maria Fisher <maria.fisher@thinkshout.com>
2024-05-16 12:20:14 -0700

    Adding new update hook.

--- modules/wri_narrative/wri_narrative.post_update.php
+++ modules/wri_narrative/wri_narrative.post_update.php
@@ -71,3 +71,66 @@ function wri_narrative_post_update_rewrite_narrative_taxonomies(&$sandbox) {
 
   $sandbox['#finished'] = ($sandbox['current'] == $start_value);
 }
+
+/**
+ * Updates the tokens in narrative taxonomies, round 2.
+ */
+function wri_narrative_post_update_rewrite_narrative_taxonomies2(&$sandbox) {
+  if (!isset($sandbox['total'])) {
+    $sandbox['total'] = Drupal::database()->select('node__field_narrative_taxonomy', 'u')
+      ->condition('u.field_narrative_taxonomy_value', '[node:field_(projects|primary_contacts):entity:link]', 'REGEXP')
+      ->fields('u')
+      ->countQuery()
+      ->execute()
+      ->fetchField();
+    $sandbox['current'] = 0;
+
+    if (empty($sandbox['total'])) {
+      $sandbox['#finished'] = 1;
+      return;
+    }
+  }
+
+  $users_per_batch = 25;
+  $start_value = $sandbox['current'];
+  $nids = Drupal::database()->select('node__field_narrative_taxonomy', 'u')
+    ->condition('u.field_narrative_taxonomy_value', '[node:field_(projects|primary_contacts):entity:link]', 'REGEXP')
+    ->condition('entity_id', $sandbox['current'], '>=')
+    ->fields('u')
+    ->orderBy('entity_id')
+    ->range(0, $users_per_batch)
+    ->execute();
+
+  if (empty($nids)) {
+    $sandbox['#finished'] = 1;
+    return;
+  }
+  // Loop through each node.
+  foreach ($nids as $result) {
+    $node = Node::load($result->entity_id);
+    if ($node->hasTranslation($result->langcode)) {
+      $node = $node->getTranslation($result->langcode);
+    }
+    $taxonomy_value = $node->field_narrative_taxonomy->getValue();
+    // Replace link strings with new values.
+    $taxonomy_value[0]['value'] = str_replace(
+      ['[node:field_primary_contacts:entity:link]',
+        '[node:field_projects:entity:link]',
+      ],
+      [ '[node:primary_contact_links]',
+      '[node:projects_links]'
+      ],
+      $taxonomy_value[0]['value']
+    );
+    $node->field_narrative_taxonomy->setValue($taxonomy_value);
+
+    // Save the node.
+    $node->save();
+    $sandbox['current'] = $result->entity_id;
+  }
+
+  \Drupal::messenger()
+    ->addMessage($sandbox['current'] . ' node last processed.');
+
+  $sandbox['#finished'] = ($sandbox['current'] == $start_value);
+}
