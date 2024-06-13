f2b346b9674183f76b9e84d1a2bf1be2836b1795
Maria Fisher <maria.fisher@thinkshout.com>
2024-06-13 14:06:29 -0700

    Issue #389 tracking submissions.

--- modules/wri_zoom/js/ap3c-track.js
+++ modules/wri_zoom/js/ap3c-track.js
@@ -2,14 +2,18 @@
  * @file
  * WRI Zoom behaviors.
  */
-(function (Drupal, ap3c) {
+(function (Drupal) {
 
   'use strict';
 
   Drupal.behaviors.wriZoomAp3cTrack = {
     attach (context, settings) {
-      ap3c.track({"v":0,settings.wriZoomAp3cTrack.email});
+      let trackEmail = settings.wriZoomAp3cTrack.email;
+      if (typeof ap3c !== "undefined") {
+        ap3c.track({"v":0,"email":trackEmail});
+        console.log(trackEmail);
+      }
     }
   };
 
-} (Drupal, ap3c));
+} (Drupal));
