23502d45260b4df175114368f3d0c3621227bd81
Maria Fisher <maria.fisher@thinkshout.com>
2024-06-13 15:30:51 -0700

    Figuring out a much simpler way to track successful submissions.

--- modules/wri_zoom/js/ap3c-track.js
+++ modules/wri_zoom/js/ap3c-track.js
@@ -2,18 +2,19 @@
  * @file
  * WRI Zoom behaviors.
  */
-(function (Drupal) {
+(function (Drupal, Cookies) {
 
   'use strict';
 
   Drupal.behaviors.wriZoomAp3cTrack = {
     attach (context, settings) {
-      let trackEmail = settings.wriZoomAp3cTrack.email;
-      if (typeof ap3c !== "undefined") {
+      let trackEmail = Cookies.get('ap3c_track_email');
+      if (trackEmail && typeof ap3c !== "undefined" && typeof ap3c.track !== "undefined") {
         ap3c.track({"v":0,"email":trackEmail});
-        console.log(trackEmail);
+        //console.log('ap3c tracked email' + trackEmail);
+        Cookies.remove('ap3c_track_email', { path: '/', domain: '.' + window.location.hostname });
       }
     }
   };
 
-} (Drupal));
+} (Drupal, Cookies));
