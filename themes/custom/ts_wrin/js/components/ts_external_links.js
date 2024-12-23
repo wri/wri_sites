48d7cded5c76a5d43428877d5a8985e958435d8d
mariacha <mariacha@gmail.com>
2024-12-23 09:54:10 -0800

    Skipping links to follow on X.

--- themes/custom/ts_wrin/js/components/ts_external_links.js
+++ themes/custom/ts_wrin/js/components/ts_external_links.js
@@ -11,7 +11,7 @@ export default function(context) {
     '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="11.3" fill="none" viewBox="0 0 17 16"><path fill="currentColor" d="M2.278 16c-.49 0-.908-.174-1.256-.522A1.712 1.712 0 0 1 .5 14.222V1.778c0-.49.174-.908.522-1.256A1.712 1.712 0 0 1 2.278 0H8.5v1.778H2.278v12.444h12.444V8H16.5v6.222c0 .49-.174.908-.522 1.256a1.712 1.712 0 0 1-1.256.522H2.278Zm4.178-4.711L5.21 10.044l8.267-8.266h-3.2V0H16.5v6.222h-1.778v-3.2L6.456 11.29Z"/></svg>';
 
   // Loop over all external links.
-  $("a[href^=http]", context).each(function() {
+  $("a[href^=http]:not('.visit-on-twitter')", context).each(function() {
     let $this = $(this);
     // If the url doesn't contain the hostname treat it as external.
     // Also count if it is an external sharing url that contains the domain as a parameter.
