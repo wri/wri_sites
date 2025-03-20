3def1ca36e67f1dc803c8881282bb1b24d1430bf
Katie Escoto <katie.escoto@thinkshout.com>
2025-03-20 14:03:17 -0400

    Syntax fixes and adding if statement to AWS command

--- .ci/pre-deploy.sh
+++ .ci/pre-deploy.sh
@@ -14,15 +14,17 @@ terminus -n auth:login --machine-token="$TERMINUS_TOKEN"
 
 
 # Wipe all content from the 'live-backup' environment
-if ! terminus multidev:delete --delete-branch $TERMINUS_SITE.live-backup; then
+if ! terminus multidev:delete --delete-branch "$TERMINUS_SITE".live-backup; then
   echo "live-backup not found. Skipping this step."
 fi
 
 # Clone the live site's database/files to a `live-backup` environment
-terminus multidev:create $TERMINUS_SITE.live live-backup
+terminus multidev:create "$TERMINUS_SITE".live live-backup
 
 # Start the process of cloning the live database into test
-terminus backup:create $TERMINUS_SITE.test && terminus env:clone-content $TERMINUS_SITE.live test -y && terminus drush $TERMINUS_SITE.test -- sapi-sc pantheon
+terminus backup:create "$TERMINUS_SITE".test && terminus env:clone-content "$TERMINUS_SITE".live test -y && terminus drush "$TERMINUS_SITE".test -- sapi-sc pantheon
 
 # If this is Flagship, pull files on AWS from the live to the dev bucket
-aws s3 sync s3://wriorg/d8/s3fs-public s3://wriorg-dev/d8/s3fs-public --acl public-read
+if [[ "$TERMINUS_SITE" == "wriflagship" ]]
+  aws s3 sync s3://wriorg/d8/s3fs-public s3://wriorg-dev/d8/s3fs-public --acl public-read
+fi
