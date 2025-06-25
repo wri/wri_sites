6588aa39d8cc7acd91500c04c9bd4d6ed79eaebb
Gabe Carleton-Barnes <gabe@thinkshout.com>
2025-06-24 18:08:40 -0700

    Move backlink functionality into events module. #394

--- modules/wri_event/js/backlink.js
+++ modules/wri_event/js/backlink.js
@@ -6,16 +6,16 @@
 
   'use strict';
 
-  Drupal.behaviors.wriZoomEventBacklink = {
+  Drupal.behaviors.wriEventBacklink = {
     attach (context, settings) {
       const queryString = window.location.search;
       const urlParams = new URLSearchParams(queryString);
-      if (urlParams.get('return')) {
+      if (urlParams.get('returnTo')) {
         // find the href with class name='event_backlink'
         var backlinks = document.getElementsByClassName('event_backlink');
         // set the value of the form element to the browser language
         for (let i = 0; i < backlinks.length; i++) {
-          backlinks[i].href = urlParams.get('return');
+          backlinks[i].href = urlParams.get('returnTo');
           backlinks[i].removeAttribute('hidden');
         }
       }
