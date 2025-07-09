029f30528de832350e22b769ef076a1d7bd96d14
komejo <komejo@gmail.com>
2025-07-09 11:06:17 -0700

    Modest refactor of facet limit JS to include links

--- themes/custom/ts_wrin/js/components/ts_facets.js
+++ themes/custom/ts_wrin/js/components/ts_facets.js
@@ -5,7 +5,6 @@
  */
 export default function(context) {
   const $ = jQuery;
-  const maxTotalSelections = 4;
 
   $(".block-facets h3")
     .off("click")
@@ -52,34 +51,72 @@ export default function(context) {
   }
 
   // Global facet limit across all blocks.
+  const maxTotalSelections = 3;
+
+  // Helper: counts total selected facets from both sets
+  function getTotalFacetCount() {
+    const checkboxCount = $(".block-facets input[type='checkbox']:checked").length;
+    const linkCount = $(".facets-widget-links a[data-drupal-facet-item-id].is-active").length;
+    return checkboxCount + linkCount;
+  }
+
+  // Show warning message
+  function showFacetLimitWarning() {
+    $(".facet-limit-warning").remove();
+
+    const translatedMsg = Drupal.t('You can select up to @max filters total, including the topic.', {
+      '@max': (maxTotalSelections),
+    });
+
+    const $msg = $(
+      `<div class="facet-limit-warning" style="color: #cd4443; font-weight: bold; margin-bottom: 1em;">
+        ${translatedMsg}
+      </div>`
+    );
+
+    $(".view-filters").append($msg);
+
+    $("html, body").animate({
+      scrollTop: $(".view-filters").offset().top - 20,
+    }, 300);
+  }
+
+  // Handle checkbox facets
   $(".facets-widget-checkbox input[type='checkbox']", context).each(function () {
     const $checkbox = $(this);
 
     $checkbox.off("click.limit").on("click.limit", function (e) {
-      // Total selected *before* this one toggles.
-      const checkedCount = $(".block-facets input[type='checkbox']:checked").length;
+      const checkedCount = getTotalFacetCount();
       const willBeChecked = $checkbox.prop("checked");
 
       if (willBeChecked && checkedCount > maxTotalSelections) {
         e.preventDefault();
         e.stopImmediatePropagation();
-
-        $(".facet-limit-warning").remove();
-
-        const translatedMsg = Drupal.t('You can select up to @max filters total.', {
-          '@max': maxTotalSelections,
-        });
-        const $msg = $(
-          `<div class="facet-limit-warning" style="color: red; font-weight: bold; margin-bottom: 1em;">
-            ${translatedMsg}
-          </div>`
-        );
-
-        $(".view-filters").append($msg);
-        $("html, body").animate({
-            scrollTop: $(".view-filters").offset().top - 20,
-          }, 300);
+        $checkbox.prop("checked", false);
+        showFacetLimitWarning();
       }
     });
   });
+
+  // Handle link facets
+  $(".facets-widget-links a[data-drupal-facet-item-id]", context).each(function () {
+    const $link = $(this);
+
+    // Only intercept inactive links (i.e., not already selected)
+    if (!$link.hasClass("is-active")) {
+      $link.off("click.limit").on("click.limit", function (e) {
+        const activeLinkCount = $(".facets-widget-links a[data-drupal-facet-item-id].is-active").length;
+        const checkboxCount = $(".block-facets input[type='checkbox']:checked").length;
+
+        const willExceedLimit = activeLinkCount === 0 && checkboxCount >= maxTotalSelections;
+
+        if (willExceedLimit) {
+          e.preventDefault();
+          e.stopImmediatePropagation();
+          showFacetLimitWarning();
+        }
+      });
+    }
+  });
+
 }
