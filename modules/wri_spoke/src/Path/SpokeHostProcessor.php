2ea8f2f0924696c01e437548c9367e5fd22e775b
Gabe Carleton-Barnes <gabe@thinkshout.com>
2025-07-14 17:54:27 -0700

    Add basic version of Event URL override. #394

---
+++ modules/wri_spoke/src/Path/SpokeHostProcessor.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\wri_spoke\Path;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\PathProcessor\OutboundPathProcessorInterface;
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
+use Symfony\Component\HttpFoundation\Request;
+
+class SpokeHostProcessor implements OutboundPathProcessorInterface {
+
+  /**
+   * @var $configFactory ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processOutbound($path, &$options = array(), Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL) {
+    try {
+      $url = Url::fromUri('internal:' .  $path);
+    }
+    catch (\Throwable $throwable) {
+      return $path;
+    }
+    if (!$url) {
+      return $path;
+    }
+
+    $routeName = $url->getRouteName();
+    if ($routeName != 'entity.node.canonical') {
+      return $path;
+    }
+
+    $params = $url->getRouteParameters();
+    if (!isset($params['node'])) {
+      return $path;
+    }
+
+    $node = $params['node'];
+    if (!($node instanceof Node) && is_numeric($node)) {
+      $node = Node::load($node);
+    }
+    if (!($node instanceof Node)) {
+      return $path;
+    }
+    if ($node->bundle() == 'event') {
+      $options['absolute'] = TRUE;
+      $options['base_url'] = $request->getScheme() . '://web.wri-hub.localhost';
+    }
+    return $path;
+  }
+}
