26b08aacaa70bbef91aba0162c672e07846c961b
Katie Escoto <katie.escoto@thinkshout.com>
2025-08-15 13:58:19 -0400

    Adjusted check-for-new-tag script

--- .ci/check-for-new-tag.sh
+++ .ci/check-for-new-tag.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-# load common functions
-script_path=$( cd "$(dirname "$0")" || exit 2; pwd -P )
-# shellcheck source=scripts/util.sh
-source "$script_path/util.sh"
+# check for latest commit used in composer.lock file
+lockfile_commit=$(composer show "wri/wri_sites" | grep '^source' | head -n 1 | awk '{print $NF}')
 
-# check for latest commit
-latest_commit=$(git rev-parse HEAD)
+# check out the main branch from wri_sites to get its latest commit
+PACKAGE_PATH='web/profiles/contrib/wri_sites'
+BRANCH_NAME='main'
+
+cd "$PACKAGE_PATH"
+
+git fetch origin
+
 # check for latest tag
-latest_tag=$(git tag --sort=-taggerdate | head -n 1)
+latest_tag=$(git tag --sort=-creatordate --merged "$BRANCH_NAME" | head -n 1)
 # pull commit associated with latest tag
 ltag_commit=$(git rev-list -n 1 "$latest_tag")
 
 # if latest tag is a descendant of the latest tag commit, return pass
-if [ $(git merge-base --is-ancestor "$ltag_commit" "$latest_commit") 0 ]; then
+if [ $(git merge-base --is-ancestor $lockfile_commit $ltag_commit) 0 ]; then
   echo "New tags found. Running pre-deploy.sh"
   source "$script_path/pre-deploy.sh"
 else
   echo "No new tags found."
 fi
+
+# move back to site folder
+cd -
 
 
