264e84a91dda829e4e493aba8bae4de83f223842
Thinkshout Automation <ci-bot@thinkshout.com>
2025-09-11 16:10:43 -0700

    PHPCS.

--- modules/wri_spoke/src/Path/SpokeHostProcessor.php
+++ modules/wri_spoke/src/Path/SpokeHostProcessor.php
@@ -77,10 +77,11 @@ class SpokeHostProcessor implements OutboundPathProcessorInterface {
       return $path;
     }
     if (isset($options['language'])) {
-        try {
-            $node = $node->getTranslation($options['language']->getId());
-        }
-        catch (\Exception $e) {}
+      try {
+        $node = $node->getTranslation($options['language']->getId());
+      }
+      catch (\Exception $e) {
+      }
     }
     if ($node->bundle() == 'event' && $node->field_hub_canonical_url->value) {
       $options['absolute'] = TRUE;
