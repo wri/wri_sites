a656885624ecf73d46c866d8e1975480b4063325
Thinkshout Automation <ci-bot@thinkshout.com>
2025-11-25 12:23:07 -0800

    Continued work on making the search results look like the old search results.

--- modules/wri_search/src/EventSubscriber/WriSearchControllerAlterSubscriber.php
+++ modules/wri_search/src/EventSubscriber/WriSearchControllerAlterSubscriber.php
@@ -4,29 +4,45 @@ declare(strict_types=1);
 
 namespace Drupal\wri_search\EventSubscriber;
 
+use Drupal\Core\Routing\RouteMatchInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * @todo Add description for this subscriber.
  */
 final class WriSearchControllerAlterSubscriber implements EventSubscriberInterface {
-
   /**
-   * Kernel request event handler.
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
    */
-  public function onKernelRequest(RequestEvent $event): void {
-    // @todo Place your code here.
+  protected $routeMatch;
+  /**
+   * Constructs a new RouteCacheContext class.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   */
+  public function __construct(RouteMatchInterface $route_match) {
+    $this->routeMatch = $route_match;
   }
 
   /**
    * Kernel response event handler.
    */
-  public function onKernelResponse(ResponseEvent $event): void {
-    // @todo Place your code here.
-    print "";
+  public function onKernelView(ViewEvent $event): void {
+    if ($this->routeMatch->getRouteName() == 'search.view_google_json_api_search') {
+      $build = $event->getControllerResult();
+      // Add wrapper around search_results.
+      $build['#theme'] = 'wri_search_results_wrapper';
+      $build['#pager'] = $build['pager'] ?? [];
+      $build['#search_form'] = $build['search_form'] ?? [];
+      $build['#search_results'] = $build['search_results'] ?? [];
+      $build['#search_results_title'] = $build['search_results_title'] ? strip_tags($build[ 'search_results_title']['#markup']) : '';
+      $event->setControllerResult($build);
+    }
   }
 
   /**
@@ -34,8 +50,7 @@ final class WriSearchControllerAlterSubscriber implements EventSubscriberInterfa
    */
   public static function getSubscribedEvents(): array {
     return [
-      KernelEvents::REQUEST => ['onKernelRequest'],
-      KernelEvents::RESPONSE => ['onKernelResponse'],
+      KernelEvents::VIEW => ['onKernelView',10],
     ];
   }
 
