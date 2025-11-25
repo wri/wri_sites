c4450d3b174f3fbd2a474105918a6dfcfc6d4a0a
Thinkshout Automation <ci-bot@thinkshout.com>
2025-11-25 10:49:21 -0800

    Getting started customizing the whole page.

---
+++ modules/wri_search/src/EventSubscriber/WriSearchControllerAlterSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\wri_search\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * @todo Add description for this subscriber.
+ */
+final class WriSearchControllerAlterSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Kernel request event handler.
+   */
+  public function onKernelRequest(RequestEvent $event): void {
+    // @todo Place your code here.
+  }
+
+  /**
+   * Kernel response event handler.
+   */
+  public function onKernelResponse(ResponseEvent $event): void {
+    // @todo Place your code here.
+    print "";
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest'],
+      KernelEvents::RESPONSE => ['onKernelResponse'],
+    ];
+  }
+
+}
