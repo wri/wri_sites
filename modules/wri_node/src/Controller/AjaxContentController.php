d25c0aedfe3fe60cc71247d72a309fc3534f8802
Thinkshout Automation <ci-bot@thinkshout.com>
2026-02-26 07:57:04 -0800

    PHPCS.

--- modules/wri_node/src/Controller/AjaxContentController.php
+++ modules/wri_node/src/Controller/AjaxContentController.php
@@ -1,26 +1,83 @@
 <?php
+
 namespace Drupal\wri_node\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\ReplaceCommand;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\node\Entity\Node;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * Loads a node's content and returns it as an Ajax response.
+ */
 class AjaxContentController extends ControllerBase {
 
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * Constructs a new AjaxContentController.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
+   *   The language manager.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $dateFormatter
+   *   The date formatter service.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entityTypeManager,
+    RendererInterface $renderer,
+  ) {
+    $this->entityTypeManager = $entityTypeManager;
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('renderer'),
+    );
+  }
+
+  /**
+   * Loads content for a node and returns it as an Ajax response.
+   */
   public function loadContent(Request $request, Node $node) {
     $view_mode = $request->query->get('view_mode') ?: 'main_content_b';
 
-    // Render the node in the specified view mode
-    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('node');
+    // Render the node in the specified view mode.
+    $view_builder = $this->entityTypeManager->getViewBuilder('node');
     $content = $view_builder->view($node, $view_mode);
-    $rendered_content = \Drupal::service('renderer')->render($content);
+    $rendered_content = $this->renderer->render($content);
 
-    // Create an Ajax response and replace a specific container on the page
+    // Create an Ajax response and replace a specific container on the page.
     $response = new AjaxResponse();
     $response->addCommand(new ReplaceCommand('.article-columns', $rendered_content));
 
     return $response;
   }
+
 }
