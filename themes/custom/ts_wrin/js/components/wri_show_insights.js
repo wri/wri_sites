42c80c14b901996da16985cee3d244b3621dc324
Thinkshout Automation <ci-bot@thinkshout.com>
2026-02-26 08:29:46 -0800

    Issue #574 simplifying logic about when to fade in content.

--- themes/custom/ts_wrin/js/components/wri_show_insights.js
+++ themes/custom/ts_wrin/js/components/wri_show_insights.js
@@ -5,19 +5,7 @@
  */
 
 export default function (context) {
-  if (context == document) {
-    const bodyClasses = context.body.classList;
-    if (bodyClasses.contains("abjs-test")) {} else {
-      let articleBody = context.querySelectorAll(
-        ".article-columns, .article-b"
-      );
-
-      articleBody.forEach(function (articleBody) {
-        articleBody.classList.add("reveal");
-      });
-    }
-  }
-  else if (context.classList.contains("article-b")) {
+  if (context != document && context.classList.contains("article-b")) {
     context.classList.add("reveal");
   }
 }
