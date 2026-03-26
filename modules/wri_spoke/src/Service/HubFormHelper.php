29aecb4f92e0eb807a87c8db399477113364d536
Thinkshout Automation <ci-bot@thinkshout.com>
2026-03-26 10:31:02 -0700

    PHPCS.

--- modules/wri_spoke/src/Service/HubFormHelper.php
+++ modules/wri_spoke/src/Service/HubFormHelper.php
@@ -1,35 +1,21 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Drupal\wri_spoke\Service;
 
-use Drupal\Component\Render\FormattableMarkup;
-use Drupal\Component\Serialization\Json;
-use Drupal\Core\Datetime\DrupalDateTime;
-use Drupal\Core\Entity\EntityFieldManagerInterface;
-use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\Language\LanguageManager;
-use Drupal\Core\Language\LanguageManagerInterface;
-use Drupal\Core\Link;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\entity_share_client\Service\FormHelper;
 use Drupal\Core\Url;
-use Drupal\entity_share\EntityShareUtility;
-use Drupal\entity_share_client\Entity\RemoteInterface;
-use Drupal\entity_share_client\Exception\ResourceTypeNotFoundException;
-use Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface;
 
 /**
  * Service to extract code out of the PullForm.
  *
  * @package Drupal\entity_share_client\Service
  */
-class HubFormHelper extends \Drupal\entity_share_client\Service\FormHelper {
+class HubFormHelper extends FormHelper {
 
   /**
-   * { inheritdoc }
+   * {@inheritdoc}
    */
   protected function getOptionLabel(array $data, array $status_info, array $entity_keys, $remote_url, $level) {
     // It's confusing that on our spokes, "View local" points to the hub.
