a7b481d1fb9f8dce8fa7c92a0de0d475c12b5b87
komejo <komejo@gmail.com>
2026-06-15 18:23:34 -0700

    WRIN 583: facets theming & config, view tweaks.

--- themes/custom/ts_wrin/js/components/ts_facets.js
+++ themes/custom/ts_wrin/js/components/ts_facets.js
@@ -7,7 +7,8 @@ export default function (context) {
   const $ = jQuery;
   $(".button.filters")
     .off("click")
-    .on("click", function () {
+    .on("click", function (e) {
+      e.preventDefault();
       $(this)
         .closest(
           ".view-resources, .view-experts-staff, .view-events, .view-paying-for-paris-resources",
